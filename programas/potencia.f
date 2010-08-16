2f37c562ef8a4d6e52754b432d13ff967c926c2b
Roberto Bonvallet <rbonvall@gmail.com>
2010-08-16 01:12:10 -0400

    Eliminar comentarios de potencia.f

--- programas/potencia.f
+++ programas/potencia.f
@@ -1,22 +1,15 @@
 program potencia
-
-    ! declaraciones
     implicit none
     real :: base, resultado
     integer :: exponente, i
 
-    ! entrada
     read *, base
     read *, exponente
 
-    ! proceso
     resultado = 1
     do i = 1, exponente
         resultado = resultado * base
     end do
 
-    ! salida
     print *, resultado
-
 end program potencia
-
