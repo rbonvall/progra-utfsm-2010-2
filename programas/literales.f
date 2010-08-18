82b2e5372867c5dec11580c7862d6f60714f352e
Roberto Bonvallet <rbonvall@gmail.com>
2010-08-18 01:22:22 -0400

    Programa literales.f

---
+++ programas/literales.f
@@ -0,0 +1,20 @@
+program literales
+    implicit none
+    integer :: n
+    real :: x
+    logical :: p
+    character :: c
+
+    n = 142857
+    n = -1234
+
+    x = 3.141592654
+    x = 6.02e23  ! notacion cientifica
+
+    p = .TRUE.
+    p = .FALSE.
+
+    c = 'a'
+    c = '#'
+    c = '!'
+end program literales
