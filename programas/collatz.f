4b13ab215dd9dadf80f7c083db3878904d023109
Roberto Bonvallet <rbonvall@gmail.com>
2010-08-23 14:06:19 -0400

    Mejoras a Collatz

--- programas/collatz.f
+++ programas/collatz.f
@@ -1,14 +1,15 @@
 program collatz
+    implicit none
     integer :: n
 
+    print *, 'Ingrese n'
     read *, n
-
     do while (n > 1)
-        print *, n
         if (mod(n, 2) == 0) then
             n = n / 2
         else
             n = 3 * n + 1
         end if
+        print *, n
     end do
 end program collatz
