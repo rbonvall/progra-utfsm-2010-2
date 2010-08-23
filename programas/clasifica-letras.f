9aea391573987303b2844d3cedeb664fad3000e7
Roberto Bonvallet <rbonvall@gmail.com>
2010-08-23 14:11:11 -0400

    Mejoras a clasificador de caracteres

--- programas/clasifica-letras.f
+++ programas/clasifica-letras.f
@@ -2,16 +2,25 @@ program clasifica_letras
     implicit none
     character :: c
 
-    print *, 'Ingrese una letra'
+    print *, 'Ingrese un caracter'
     read *, c
 
     select case (c)
         case ('a', 'e', 'i', 'o', 'u')
-            print *, c, ' es una vocal'
+            print *, c, ' es una vocal minuscula'
+        case ('A', 'E', 'I', 'O', 'U')
+            print *, c, ' es una vocal mayuscula'
         case ('b', 'c', 'd', 'f', 'g', 'h', 'j', &
               'k', 'l', 'm', 'n', 'p', 'q', 'r', &
               's', 't', 'v', 'w', 'x', 'y', 'z')
-            print *, c, ' es una consonante'
+            print *, c, ' es una consonante minuscula'
+        case ('B', 'C', 'D', 'F', 'G', 'H', 'J', &
+              'K', 'L', 'M', 'N', 'P', 'Q', 'R', &
+              'S', 'T', 'V', 'W', 'X', 'Y', 'Z')
+            print *, c, ' es una consonante mayuscula'
+        case ('0', '1', '2', '3', '4', &
+              '5', '6', '7', '8', '9')
+            print *, c, ' es un digito'
         case default
             print *, c, ' es un simbolo'
     end select
