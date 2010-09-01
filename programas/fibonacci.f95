a070c42fd7ec0db5a0bf00ad4837b79ad151c4cc
Roberto Bonvallet <rbonvall@gmail.com>
2010-09-01 16:04:46 -0400

    Fibonacci hasta n no más

--- programas/fibonacci.f95
+++ programas/fibonacci.f95
@@ -1,14 +1,16 @@
 program fibonacci
     implicit none
-    integer :: anterior, actual, suma, k
-    integer, parameter :: N = 40
+    integer :: anterior, actual, suma, k, n
+
+    print *, 'Cuantos numeros de Fibonacci desea?'
+    read *, n
     
     print *, 0
     print *, 1
 
     anterior = 0
     actual = 1
-    do k = 1, N
+    do k = 3, n
         suma = anterior + actual
         anterior = actual
         actual = suma
