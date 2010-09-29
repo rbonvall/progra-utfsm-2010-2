408a907cc0f88e3a0d5253640745a0fb77ed86a1
Roberto Bonvallet <rbonvall@gmail.com>
2010-09-29 15:42:28 -0400

    Restaurar espacios en programa

--- programas/ruteo-subprogramas.f95
+++ programas/ruteo-subprogramas.f95
@@ -1,13 +1,16 @@
 program ruteo_diabolico
     implicit none
     integer :: a, b, c, d, e
+
     a = 1
     b = 2
     c = 3
     d = f(a, b)
     e = f(b, a)
     print *, a, b, c, d, e
+
 contains
+
     function f(a, b) result(y)
         integer, intent(in) :: a
         integer, intent(in out) :: b
@@ -16,10 +19,12 @@ contains
         c = c - 1
         y = a * g(b) / 2
     end function f
+
     function g(c) result (e)
         integer, intent(in out) :: c
         integer :: e 
         c = c + 1
         e = 2 * c
     end function g
+
 end program ruteo_diabolico
