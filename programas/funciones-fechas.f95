c7c7862f72a29e216b71d8350f5a930ba20f20fe
Roberto Bonvallet <rbonvall@gmail.com>
2010-10-18 23:22:34 -0300

    Hacer que funciones-fechas calcule edad en días

--- programas/funciones-fechas.f95
+++ programas/funciones-fechas.f95
@@ -10,18 +10,15 @@ program funciones_fechas
     integer, dimension(12), parameter :: dias_mes = (/31, 28, 31, 30, &
                                                       31, 30, 31, 31, &
                                                       30, 31, 30, 31/)
-    type(fecha) :: f
+    type(fecha) :: nacimiento, hoy
 
-    do
-        print *, 'Ingrese una fecha:'
-        read *, f
-        if (es_fecha_valida(f)) then
-            print *, 'El dia siguiente es: ', dia_siguiente(f)
-        else
-            print *, 'Fecha invalida'
-        end if
-        print *
-    end do
+    print *, 'Ingrese su fecha de nacimiento:'
+    read *, nacimiento
+
+    print *, 'Ingrese la fecha de hoy:'
+    read *, hoy
+
+    print *, 'Usted tiene', dias_entre(nacimiento, hoy), 'dias de edad'
 
 contains
 
@@ -32,6 +29,7 @@ contains
         v = (1 <= f % mes .and. f % mes <= 12 .and.  &
              1 <= f % dia .and. f % dia <= dias_mes(f % mes))
     end function es_fecha_valida
+
 
     function dia_siguiente(f) result (fs)
         type(fecha), intent(in) :: f
@@ -48,6 +46,51 @@ contains
             fs % mes = 1
         end if
     end function dia_siguiente
+
+
+    function dias_entre(f_inicio, f_fin) result(n)
+        type(fecha), intent(in) :: f_inicio, f_fin
+        type(fecha) :: f
+        integer :: n
+
+        n = 0
+
+        ! asegurarse que las fechas estén en el orden correcto
+        if (.not. fecha_menor_que(f_inicio, f_fin)) then
+            return   ! salir de la función
+        end if
+
+        ! avanzar desde el dia inicial al final llevando la cuenta
+        f = f_inicio
+        do while (.not. fecha_iguales(f, f_fin))
+            f = dia_siguiente(f)
+            n = n + 1
+        end do
+    end function dias_entre
+
+
+    function fecha_menor_que(f1, f2) result(m)
+        type(fecha), intent(in) :: f1, f2
+        logical :: m
+        
+        if (f1 % anno /= f2 % anno) then
+            m = (f1 % anno < f2 % anno)
+        else if (f1 % mes /= f2 % mes) then
+            m = (f1 % mes < f2 % mes)
+        else
+            m = (f1 % dia < f2 % dia)
+        end if
+    end function fecha_menor_que
+
+
+    function fecha_iguales(f1, f2) result(iguales)
+        type(fecha), intent(in) :: f1, f2
+        logical :: iguales
+
+        iguales = (f1 % dia == f2 % dia .and. &
+                   f1 % mes == f2 % mes .and. &
+                   f1 % anno == f2 % anno)
+    end function fecha_iguales
 
 end program funciones_fechas
 
